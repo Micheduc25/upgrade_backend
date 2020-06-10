7ade6afe1190c1fd86c936701c8ecc195628fffc
viryl15 <viryl1515@gmail.com>
2020-06-10 11:37:20 +0100

    some update

--- database/seeds/User1sLabelSeeder.php
+++ database/seeds/User1sLabelSeeder.php
@@ -40,15 +40,15 @@ class User1sLabelSeeder extends Seeder
             'username'=>'rodyl',
             'password'=>'root',
             'role'=>'super_administrator',
-            'updated_tasks'=>[],
-            'deleted_tasks'=>[],
+            'updated_tasks'=>null,
+            'deleted_tasks'=>null,
         ]);
         $u2 = User1::create([
             'username'=>'joy',
             'password'=>'1234',
             'role'=>'supervisor',
-            'updated_tasks'=>[],
-            'deleted_tasks'=>[],
+            'updated_tasks'=>null,
+            'deleted_tasks'=>null,
         ]);
 
         $u1->task()->save($t1);
