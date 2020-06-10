2ccde2f2fa1e00f6f561a1ff5c6ff16c71ec6cc5
Mich25Educ <ndjockjunior@gmail.com>
2020-06-10 11:30:53 +0100

    modif

--- database/seeds/User1sLabelSeeder.php
+++ database/seeds/User1sLabelSeeder.php
@@ -35,20 +35,20 @@ class User1sLabelSeeder extends Seeder
             'tstate'=>false,
             'synchronized'=>true
         ]);
-
+        
         $u1 = User1::create([
             'username'=>'rodyl',
             'password'=>'root',
             'role'=>'super_administrator',
-            'updated_tasks'=>[],
-            'deleted_tasks'=>[],
+            'updated_tasks'=>[""],
+            'deleted_tasks'=>[""],
         ]);
         $u2 = User1::create([
             'username'=>'joy',
             'password'=>'1234',
             'role'=>'supervisor',
-            'updated_tasks'=>[],
-            'deleted_tasks'=>[],
+            'updated_tasks'=>[""],
+            'deleted_tasks'=>[""],
         ]);
 
         $u1->task()->save($t1);
